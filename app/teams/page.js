e65b52022495ffe9f9068fb8e179cb39e2bf9194
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-11-06 17:10:43 +0545

    team page done

---
+++ app/teams/page.js
@@ -0,0 +1,128 @@
+"use client";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
+import SectionHeader from '@/components/SectionHeader';
+import ContactSection from "@/components/ContactSection";
+
+const teamData = [
+  {
+    position: 'President',
+    members: [
+      {
+        name: 'Prashant Kafle',
+        image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487',
+        designation: 'President'
+      }
+    ]
+  },
+  {
+    position: 'Vice Presidents & Secretaries',
+    members: [
+      { name: 'Hero', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Vice President' },
+      { name: 'Khai', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Vice President' },
+      { name: 'Kaka', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Secretary' },
+      { name: 'Sanket Shiwakoti', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Vice Secretary' },
+      { name: 'No Idea', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Treasurer' },
+      { name: 'Utsav Dahal', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Vice Secretary' }
+    ]
+  },
+  {
+    position: 'Executive Members',
+    members: [
+      { name: 'Kiran Khatiwada', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Bibika Shrestha', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Aakriti Bhandari', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Koshish Neupane', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Anup Upreti', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Nimisha Neupane', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Neetu Magar', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' },
+      { name: 'Namrata Nepal', image: 'https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/409778113_1555884088511592_5998785517839366263_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=P4J4gljcCXMQ7kNvgE_fyOj&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AdlH4S7YDJAaoeNSUXcZhMD&oh=00_AYB4wLfIBLOo90ZP0I3xZkuqMhIBa8LHcHyYQtzNND2lDQ&oe=67310487', designation: 'Member' }
+    ]
+  }
+];
+
+const TeamsPage = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with a duration of 1000ms
+  }, []);
+
+  return (
+    <>
+    <div className="max-w-6xl mx-auto my-10 text-center bg-white">
+      {/* Header Section */}
+      <SectionHeader title="Meet Our Team" subtitle="The leadership behind BCA Association MMC" />
+
+      {/* President Section */}
+      <div className="mb-12" data-aos="fade-up">
+        {teamData.slice(0, 1).map((team, index) => (
+          <div key={index} className="mb-10">
+            <h2 className="text-3xl font-bold text-blue-900">{team.position}</h2>
+            <div className="flex justify-center gap-6 mt-6">
+              {team.members.map((member, idx) => (
+                <div key={idx} className="text-center">
+                  <img
+                    src={member.image}
+                    alt={member.name}
+                    className="rounded-full w-40 h-40 mx-auto"
+                    data-aos="zoom-in"
+                  />
+                  <p className="mt-2 text-lg font-medium text-blue-900">{member.name}</p>
+                  <p className="text-md text-gray-600">{member.designation}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Vice Presidents & Secretaries Section */}
+      <div className="mb-12" data-aos="fade-up">
+        {teamData.slice(1, 2).map((team, index) => (
+          <div key={index} className="mb-10">
+            <h2 className="text-2xl font-semibold text-blue-900">{team.position}</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-6">
+              {team.members.map((member, idx) => (
+                <div key={idx} className="text-center" data-aos="flip-right">
+                  <img
+                    src={member.image}
+                    alt={member.name}
+                    className="rounded-full w-32 h-32 mx-auto"
+                  />
+                  <p className="mt-2 text-lg font-medium text-blue-900">{member.name}</p>
+                  <p className="text-md text-gray-600">{member.designation}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Executive Members Section */}
+      <div data-aos="fade-up">
+        {teamData.slice(2).map((team, index) => (
+          <div key={index}>
+            <h2 className="text-2xl font-semibold text-blue-900">{team.position}</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-6">
+              {team.members.map((member, idx) => (
+                <div key={idx} className="text-center" data-aos="flip-left">
+                  <img
+                    src={member.image}
+                    alt={member.name}
+                    className="rounded-full w-32 h-32 mx-auto"
+                  />
+                  <p className="mt-2 text-lg font-medium text-blue-900">{member.name}</p>
+                  <p className="text-md text-gray-600">{member.designation}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    <ContactSection/>
+    </>
+  );
+};
+
+export default TeamsPage;
--- components/EventSection.js
+++ components/EventSection.js
@@ -1,5 +1,3 @@
-
-
 "use client";
 import React, { useEffect } from 'react';
 import SectionHeader from './SectionHeader';
@@ -23,42 +21,42 @@ const EventSection = () => {
       title: "BCA RoadMap 2080", 
       description: "Join us for the largest tech conference of the year, showcasing the latest advancements in technology and innovation.", 
       date: "2024-11-05", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/412980447_374795915096373_7762907625877291957_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=li9FxM_H5d8Q7kNvgEtAXhv&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AxqeoD8Rbs28JSpF6wMb7d7&oh=00_AYCgXIkfeVMwHf5RUELmJhozeeELqnmK-R4NlRjL7qKuCA&oe=6731258B" 
     },
     { 
       id: '2',
       title: "Football- BCA vs CSIT", 
       description: "Researchers have made a groundbreaking discovery in artificial intelligence, pushing the boundaries of what's possible.", 
       date: "2024-11-04", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/428610122_411284284780869_3931799650956470497_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=-XQ0hkWTKdIQ7kNvgHzIvzi&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AIj9xoZaPlN6WGgdEQ-UTt2&oh=00_AYAnwC19-j_Al8sjSm658fGXevqv80rGjTUJTqId7VVDjw&oe=67311F00" 
     },
     { 
       id: '3',
       title: "Football- BCA vs BBA", 
       description: "Several tech startups received significant funding to continue developing revolutionary products and services.", 
       date: "2024-11-03", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/428620888_411910964718201_4938317840983910265_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AJyu18CmvG4Q7kNvgHQrObI&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AXElPjQgQf61AibM-1-Ouef&oh=00_AYAelyicQsW0I7tcH6UOmOn1v1vZrquNtyjvvdD1HOWZww&oe=6731017D" 
     },
     { 
       id: '4',
-      title: "Workshop On React", 
+      title: "Workshop On IOT", 
       description: "How blockchain technology is transforming the financial industry and what it means for the future of money.", 
       date: "2024-11-02", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image:"https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/454792557_513398211236142_9213939088180499612_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=wWpDLhHq4Y4Q7kNvgHnh7k8&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=A6VcEGa7QiQwPhHsMyl3e6X&oh=00_AYBMC9jN4NlI5F970BZ2XaPm23_l6z3cAEGUcA3fxzdq_A&oe=6731170C"
     },
     { 
       id: '5',
-      title: "Workshop on Next", 
+      title: "Entrance Mock Test", 
       description: "A look at the latest innovations in climate technology aimed at tackling global environmental challenges.", 
       date: "2024-11-01", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/457403746_527609173148379_1226154927446818308_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=aszfhN9aNAcQ7kNvgGTE2Se&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AzmmcV3rkEy2F_eTKn7kqBt&oh=00_AYCYrit2h2lcVWSIeZvMD-RLW86w4PW0UZf8vMX-Nnqvwg&oe=673135A6" 
     },
     { 
       id: '6',
-      title: "Hackathon", 
+      title: "Hackathon Participation", 
       description: "Exploring the future of remote work, automation, and AI in the workplace.", 
       date: "2024-10-31", 
-      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/465672387_574015495174413_12854603700089630_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AF-_7SHZavcQ7kNvgGqkuJP&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=AyXDll8DY6-EW7TGGtfa4lG&oh=00_AYCEeITo6MaF8bFyp9IjPCkQjb_6legrF4O3yAuw8vv08w&oe=67302425" 
+      image: "https://scontent.fbdp2-1.fna.fbcdn.net/v/t39.30808-6/460736410_541702915072338_8943805017178565975_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=HIqtnRvKQ9kQ7kNvgERYY8b&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=Ahr8b25s-ISmQ2QFgP3DSXt&oh=00_AYBiKlycRA338dEzO5fyzjAQj5IJ3rOsW06AR7oNkuav_Q&oe=67310528" 
     },
   ];
 
@@ -66,7 +64,8 @@ const EventSection = () => {
     <div className="max-w-6xl mx-auto">
       <SectionHeader title="Events" />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-        {eventData.map((newsItem, index) => (
+        {/* Use slice to only show the first 6 events */}
+        {eventData.slice(0, 6).map((newsItem, index) => (
           <div key={index} className="px-2 m-5" data-aos="fade-up"> {/* AOS animation applied here */}
             <Link href={`/news/${index}`}> {/* Use dynamic routing */}
               <News
@@ -84,4 +83,3 @@ const EventSection = () => {
 }
 
 export default EventSection;
-
